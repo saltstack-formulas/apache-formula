687d627e1b5162a4a0e89a59a9a6d55eaf4dfec1
removedporn <86824510+removedporn@users.noreply.github.com>
2021-07-21 19:56:35 +0800

    Delete commitlint.config.js

--- commitlint.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    extends: ['@commitlint/config-conventional'],
-    rules: {
-        'body-max-line-length': [2, 'always', 120],
-        'footer-max-line-length': [2, 'always', 120],
-        'header-max-length': [2, 'always', 72],
-    },
-};
